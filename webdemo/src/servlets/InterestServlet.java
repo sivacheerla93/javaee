d5bc9e398fbaf7f4b02c7a559c6591879adf5ac4
Siva Cheerla <sivacheerla@live.com>
2019-12-30 22:13:18 +0530

    30/12/19

---
+++ webdemo/src/servlets/InterestServlet.java
@@ -0,0 +1,38 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/interest")
+public class InterestServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String input = request.getParameter("amount");
+		if (input != null) {
+			double amount = Double.parseDouble(input);
+			double interest = amount * 0.125;
+			out.printf("<html><h1>Interest = %f</h1></html>", interest);
+		} else {
+			out.println("<h3>Sorry! Missing amount!</h3>");
+		}
+		out.close();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+}
